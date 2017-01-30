084a036bba6999251c7db249404acff599b6dd7a
1135586 <1135586@gmail.com>
2017-01-30 19:12:33 +0700

    ide_commit_ok

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,6 @@
-/**
- * Created by root on 30.01.17.
- */
 public class Test {
     public static void main(String[] args) {
+        System.out.println("test");
         System.out.println("test");
     }
 }
